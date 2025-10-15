c086aac38466f7782cd5d74d1982bb82615921c8
Zhuochen “Ary” LI <138096419+AryFinch@users.noreply.github.com>
2025-10-15 20:27:43 +0800

    main structure and github set up
    主要结构以及仓库的搭建

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Mini_Project-Team_Allocation_Simulator-NTU-SC1003
+
+
+# Li Zhuochen
+源文件在src中,刚刚写好所有结构,注释还没有来得及写
+打开src/v0.0.1-1015
+
+由于时间匆忙,暂时先写这么多
---
+++ src/v0.0.1-1015/team_allocator.py
@@ -0,0 +1,13 @@
+from student import Student
+from team import Team
+
+class TeamAllocator:
+    ##############################################
+    #
+
+    #
+    def __init__(self):
+        pass;
+
+    def allocate_students_into_teams(self,original_students):
+        pass;
